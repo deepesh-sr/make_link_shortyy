65400da30ee32b9babdcc7077d74038186db3698
deepesh-sr <220714100078@cutm.ac.in>
2025-11-03 12:33:56 +0530

    route setup , server creation.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::error::Error;
 
+use axum::routing::{get, Route};
+use axum::Router;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
+
+use crate::routes::health;
 mod routes;
 
 #[tokio::main]
@@ -19,6 +23,25 @@ async fn main()-> Result<(),Box<dyn Error>> {
     .with(tracing_subscriber::fmt::layer())  // Formats log messages nicely for console output (human-readable format)
     .init();  // Activates the logging system (makes it the global logger)
 
+    //creating a new route
+    let app = Router::new()
+            .route("/health", get(health));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.3000")
+            .await
+            .expect("Couldnot initialise TCPlisnter");
+
+    // log print the local address ( IP address + PORT )
+    tracing::debug!(
+        "listening on {}",listener
+        .local_addr()
+        .expect("could not convert listner address to local address")
+    );
+
+    //creating the server
+    axum::serve(listener, app)
+        .await
+        .expect("Could not succesfully create server");
 
     Ok(())
 }
